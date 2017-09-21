e1cd7c9b8f4a4253c10af6f73d855530dac291b2
kenny <claudio.h@ilnet.ch>
2017-09-21 15:30:59 +0200

    init

---
+++ server/server.js
@@ -0,0 +1,13 @@
+const path = require('path');
+const express = require('express');
+
+const publicPath = path.join(__dirname, '../public');
+
+var app = express();
+var port = process.env.PORT || 3000;
+
+app.use(express.static(publicPath));
+
+app.listen(3000, () => {
+    console.log(`started at port ${port}`);
+});
